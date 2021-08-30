26d91c84724a7168d1f916be5bd9c8aa7db057cd
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-31 02:38:02 +0530

    [Ankit] Add: Binary Search Tree as given in Figure

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,7 +11,18 @@ namespace BinarySearchTree
             tree.Insert(56);
             tree.Insert(30);
             tree.Insert(70);
+            tree.Insert(22);
+            tree.Insert(40);
+            tree.Insert(11);
+            tree.Insert(3);
+            tree.Insert(16);
+            tree.Insert(60);
+            tree.Insert(95);
+            tree.Insert(65);
+            tree.Insert(63);
+            tree.Insert(67);
             tree.DisplayTree();
+
         }
     }
 }
